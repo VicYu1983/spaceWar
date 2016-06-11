5832cdce2b7b293de9ecdfed068a26ac60d665a6
vic <finalsteve@hotmail.com>
2016-06-11 12:52:43 +0800

    update

---
+++ src/Main/Assets/vic/script/View.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class View : MonoBehaviour {
+	List<GameObject> gs = new List<GameObject>();
+
+	public GameObject pre_player;
+
+	// Use this for initialization
+	void Start () {
+		CreatePlayer ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CreatePlayer(){
+		GameObject p = Instantiate (pre_player);
+		p.transform.parent = this.transform;
+		p.name = "player";
+		gs.Add (p);
+	}
+}
