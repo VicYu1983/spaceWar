1cb5928b93aad1f1d406680de27be383903dfbbb
HanYu <hanyushome@gmail.com>
2016-06-26 14:24:01 +0800

    no message

--- src/Main/Assets/han/ProjectV/Board.cs
+++ src/Main/Assets/han/ProjectV/Board.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
 
 namespace ProjectV.Model
 {
 	public class Board
 	{
-		
+		Piece[][] pieces;
+
+		public Piece[][] Pieces {
+			get{ return pieces; }
+			set{ pieces = value; }
+		}
+
+		public Vector2 Size {
+			get{ return new Vector2 (10, 10); }
+		}
 	}
 }
 
---
+++ src/Main/Assets/han/ProjectV/Piece.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProjectV.Model
+{
+	public enum PieceShape {
+		Unknown, Rect, Circle, Triangle, RRect, RCircle, RTriangle
+	}
+
+	public class Piece{
+		PieceShape shape;
+		public PieceShape Shape { 
+			get{ return shape; }
+			set{ shape = value; }
+		}
+	}
+}
+
