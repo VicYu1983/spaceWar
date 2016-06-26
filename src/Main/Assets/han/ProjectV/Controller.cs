b5bff3c81b1c6dd9e14f2996298f980e7b9ca77c
HanYu <hanyushome@gmail.com>
2016-06-26 16:04:12 +0800

    no message

---
+++ src/Main/Assets/han/ProjectV/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace ProjectV.Model
+{
+	public class Controller : MonoBehaviour, IView
+	{
+		public IModel model = new Model();
+		public IView view;
+
+		public IModel Model{ get{ return model; } set{ } }
+
+		void Start(){
+			view = this;
+			view.Model = model;
+
+			model.StartGame (0);
+		}
+		void OnDestroy(){
+			
+		}
+	}
+}
+
---
+++ src/Main/Assets/han/ProjectV/Model.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace ProjectV.Model
+{
+	public class Model : IModel
+	{
+		Board board = new Board();
+		public void StartGame (int level){
+			board.Pieces = new Piece[][] {
+				new Piece[]{new Piece(PieceShape.Circle), new Piece(PieceShape.Circle),new Piece(PieceShape.Rect)},
+				new Piece[]{new Piece(PieceShape.Triangle), new Piece(PieceShape.Triangle),new Piece(PieceShape.Circle)},
+				new Piece[]{new Piece(PieceShape.Circle), new Piece(PieceShape.Circle),new Piece(PieceShape.Circle)}
+			};
+		}
+		public void DestroyGame(){
+			
+		}
+		public Piece[][] Pieces{ get { return board.Pieces; } }
+		public CheckPathResult CheckPath(List<Vector2> path){
+			CheckPathResult result = new CheckPathResult();
+			Alg.CheckPath (0, board, path, out result.shape, out result.path, out result.neighbors);
+			return result;
+		}
+	}
+}
+
