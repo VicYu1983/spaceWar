469d8593adac5bbf68e54601b0a3f9d41531e1f4
HanYu <hanyushome@gmail.com>
2016-07-07 13:15:39 +0800

    no message

--- src/Main/Assets/han/WalkingDeadInDown/InputManager.cs
+++ src/Main/Assets/han/WalkingDeadInDown/InputManager.cs
@@ -2,12 +2,34 @@
 using Han.Util;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 namespace WalkingDeadInDown.Model
 {
-	public class InputManager : ITagManagerListener
+	public class InputManager : MonoBehaviour, ITagManagerListener
 	{
 		List<GameObject> cantouchs = new List<GameObject>();
+		FireSystem fireSystem;
+		//Subject ontouch = new Subject();
+
+		void Update(){
+			if (Input.touchCount > 0 ) {
+				if( Input.GetTouch(0).phase == TouchPhase.Began ){
+					Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch (0).position);
+					foreach (GameObject go in cantouchs) {
+						Collider col = go.GetComponent<Collider> ();
+						if (col != null) {
+							RaycastHit hit;
+							col.Raycast (ray, out hit, 100);
+						}
+					}
+				}
+				if (Input.GetTouch (0).phase == TouchPhase.Stationary || Input.GetTouch (0).phase == TouchPhase.Moved) {
+					
+				}
+			}
+		}
+
 
 		public ITagManager TagManager{ set; get; }
 
@@ -15,11 +37,17 @@ namespace WalkingDeadInDown.Model
 			if (obj.Belong.GetComponent<CanTouch> () != null) {
 				cantouchs.Add (obj.Belong);
 			}
+			if (obj.Tag == "playerFireSystem") {
+				fireSystem = obj.Belong.GetComponent<FireSystem> ();
+			}
 		}
 
 		public void OnUnManage(ITagObject obj){
 			if (obj.Belong.GetComponent<CanTouch> () != null) {
 				cantouchs.Remove (obj.Belong);
+			}
+			if (obj.Tag == "playerFireSystem") {
+				fireSystem = null;
 			}
 		}
 	}
